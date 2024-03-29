e51f51aca9842676507e865dda612165794d4003
Matheus Ladislau <matheus.ladislaudesenv@gmail.com>
2024-03-29 03:29:40 -0300

    C file

---
+++ hangmangame.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+
+	char palavrasecreta[20];
+	sprintf(palavrasecreta, "MELANCIA");
+
+	int acertou = 0;
+	int enforcou = 0;
+
+	char chutes[26];
+	int tentativas = 0;
+
+	do {
+		for (int i = 0; i < strlen(palavrasecreta); i++) {
+			int achou = 0;
+
+			for (int j = 0; j < tentativas; j++) {
+				if (chutes[j] == palavrasecreta[i]){
+					achou = 1;
+					break;
+				}
+			}
+
+			if (achou)
+				printf("%c ", palavrasecreta[i]);
+			else
+				printf("_ ");
+		}
+		printf("\n");
+
+		char chute;
+		scanf(" %c", &chute);
+
+		chutes[tentativas] = chute;
+		tentativas++;
+	} while(!acertou && !enforcou);
+
+	return 0;
+}
