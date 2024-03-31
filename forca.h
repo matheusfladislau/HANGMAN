457df7513e9c97b6134c03872be99862a641a497
Matheus Ladislau <matheus.ladislaudesenv@gmail.com>
2024-03-31 19:33:45 -0300

    Assinaturas e banco

---
+++ forca.h
@@ -0,0 +1,15 @@
+#define TAMANHO_PALAVRA 20
+#define LIMITE_ERROS 5
+#define LIMITE_CHUTES 50
+
+void clearscreen();
+void abertura();
+void adicionapalavra();
+void escolhepalavra();
+int jachutou(char letra);
+void desenhaforca();
+void chuta();
+int acertou();
+int letraexiste(char letra);
+int chuteserrados();
+int enforcou();
